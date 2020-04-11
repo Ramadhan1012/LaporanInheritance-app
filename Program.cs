0cae2d72d1b8c28dedf0693ecc3fd9836eda4a90
Ramadhan1012 <62045674+Ramadhan1012@users.noreply.github.com>
2020-04-11 14:02:55 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ramadhan", 18);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Dwi", 30, "191128", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Saputra", 18, "2851", "ramadhandwisaputra.1012@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
